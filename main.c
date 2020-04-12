c5846dfcd212fe8024969266a7e7509fc8e09bf0
makkasha <akkash-a@hm.edu>
2020-04-12 14:45:03 +0200

    add a function to print a string array

--- main.c
+++ main.c
@@ -1,33 +1,13 @@
 #include <stdio.h>
 #include "string.h"
 
-char *words[] = {"Zero", "One", "Two", "Three"};
-char *str = "Hello";
-char str_2[] = "World";
-
-
 int main() {
 
-    char *pc = NULL;
-    char **ppc = NULL;
+    char *words[] = {"Zero", "One", "Two", "Three"};
+    char *str = "Hello";
+    char str_2[] = "World";
 
-    ppc = words;
-
-    for (int i = 0; i < sizeof(words) / sizeof(words[0]); i++) {
-        ppc = words + i;
-        pc = *ppc;
-        while (*pc != 0) {
-            printf("%c", *pc);
-            pc += 1;
-        }
-        if (i == (sizeof(words) / sizeof(words[0]) - 1)) {
-            printf("\n");
-        } else {
-            printf(", ");
-        }
-
-    }
-
+    print_array_string(words);
     print_string(str);
     print_string(str_2);
     return 0;
--- string.c
+++ string.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 
-int print_string(char *string) {
+int print_string(char* string) {
     char *pc = NULL;
 
     pc = string;
@@ -15,4 +15,26 @@ int print_string(char *string) {
     }
     printf("\n");
     return 0;
+}
+
+int print_array_string(char** string){
+    char *pc = NULL;
+    char **ppc = NULL;
+
+    ppc = string;
+
+    for (int i = 0; i < 4; i++) {
+        ppc = string + i;
+        pc = *ppc;
+        while (*pc != 0) {
+            printf("%c", *pc);
+            pc += 1;
+        }
+        if (i == (3)) {
+            printf("\n");
+        } else {
+            printf(", ");
+        }
+
+    }
 }
